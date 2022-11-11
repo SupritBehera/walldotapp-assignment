8515edf60147849e169f6b19b8c36f3df466ef46
Suprit Behera <supritb12@gmail.com>
2022-11-11 15:38:21 +0530

    complete nft card

--- src/components/NFTCard.jsx
+++ src/components/NFTCard.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import EthVector from "../assets/images/EthVector.svg";
+
 import NFTImage1 from "../assets/images/NFTImage.svg";
 
 const NFTCard = () => {
@@ -17,10 +19,40 @@ const NFTCard = () => {
       <RootContainer>
         <Column>
           <img src={nftCardDetails.profilePicture} />
-          <MonthsContainer>{`${nftCardDetails.months} months`}</MonthsContainer>
+          <MonthsContainer>{`${nftCardDetails.months} Months`}</MonthsContainer>
         </Column>
-        <Column>
+        <Column margin="0px 5% 0px 3%">
           <Title>{`${nftCardDetails.name} #${nftCardDetails.id}`}</Title>
+          <BoughtAndFloorContainer>
+            <Subtitle>Bought</Subtitle>
+            <Subtitle white flexEnd>
+              <span
+                style={{ marginRight: 5 }}
+              >{`${nftCardDetails.bought}`}</span>
+              <span>
+                <img src={EthVector} />
+              </span>
+            </Subtitle>
+          </BoughtAndFloorContainer>
+          <BoughtAndFloorContainer margin="7% 0px 0px 0px">
+            <Subtitle>Bought</Subtitle>
+            <Subtitle white flexEnd>
+              <span
+                style={{ marginRight: 5 }}
+              >{`${nftCardDetails.bought}`}</span>
+              <span>
+                <img src={EthVector} />
+              </span>
+              {nftCardDetails.percentChange &&
+              nftCardDetails.percentChange >= 0 ? (
+                <PercentChangeSpan>{`+${nftCardDetails.percentChange}%`}</PercentChangeSpan>
+              ) : (
+                <PercentChangeSpan
+                  red
+                >{`-${nftCardDetails.percentChange}%`}</PercentChangeSpan>
+              )}
+            </Subtitle>
+          </BoughtAndFloorContainer>
         </Column>
       </RootContainer>
     </>
@@ -38,9 +70,39 @@ const RootContainer = styled.div`
   margin-top: 35%;
 `;
 
+const Title = styled.div`
+  font-family: "Sharp Grotesk Regular";
+  font-weight: 550;
+  font-size: 0.9rem;
+  color: #fafafa;
+  margin-top: 15%;
+  margin-bottom: 9%;
+`;
+
+const Subtitle = styled.div`
+  display: flex;
+  align-items: center;
+  text-align: baseline;
+  justify-content: center;
+  font-family: "Sharp Grotesk Regular";
+  font-weight: 550;
+  font-size: 0.8rem;
+  color: #a2a2a2;
+  color: ${(props) => (props.white ? "#FAFAFA" : "#a2a2a2")};
+  align-self: ${(props) => (props.flexEnd ? "flex-end" : "flex-start")};
+`;
+
 const Row = styled.div`
   display: flex;
   flex-direction: row;
+  margin: ${(props) => (props.margin ? props.margin : 0)};
+  flex-wrap: ${(props) => (props.wrap ? "wrap" : "nowrap")};
+`;
+
+const BoughtAndFloorContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
   margin: ${(props) => (props.margin ? props.margin : 0)};
   flex-wrap: ${(props) => (props.wrap ? "wrap" : "nowrap")};
 `;
@@ -68,6 +130,15 @@ const MonthsContainer = styled.div`
   margin-top: -13%;
   margin-right: 3px;
   backdrop-filter: blur(6px);
+`;
+
+const PercentChangeSpan = styled.div`
+  color: ${(props) => (props.red ? "#E97451" : " #19ed7b")};
+  color: #19ed7b;
+  font-family: "Sharp Grotesk Regular";
+  font-size: 0.75rem;
+  font-weight: 400;
+  margin-left: 4px;
 `;
 
 export default NFTCard;
