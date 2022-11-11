749fd7de1cae64c045d3321ba710ab658aa71776
Suprit Behera <supritb12@gmail.com>
2022-11-11 12:54:23 +0530

    complete twitter handle, website and description

--- src/components/LeftPane/LeftPane.jsx
+++ src/components/LeftPane/LeftPane.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import ProfileIcon from "../../assets/images/ProfileIcon.svg";
+import TwitterIcon from "../../assets/images/twitter-icon.svg";
+import GlobeIcon from "../../assets/images/Vectorglobe-final.svg";
 
 const LeftPane = () => {
   const user = {
@@ -65,7 +67,7 @@ const LeftPane = () => {
     <>
       <RootContainer>
         <Row>
-          <Column margin="22px 6px 10px 22px">
+          <Column margin="0px 6px 10px 0px">
             <img src={ProfileIcon} alt="profile icon" />
             <LevelDiv>{`LVL ${user.level}`}</LevelDiv>
           </Column>
@@ -73,6 +75,31 @@ const LeftPane = () => {
             <Title>{`${user.name}`}</Title>
             <Subtitle>{`${user.shortDesc}`}</Subtitle>
           </Column>
+        </Row>
+        {/* <LevelDiv>{`LVL ${user.level}`}</LevelDiv> */}
+        {/* <div
+          style={{
+            display: "flex",
+            height: 100,
+            width: 100,
+            backgroundColor: "white",
+          }}
+        ></div> */}
+        {/* <TwitterIcon /> */}
+        {/* <TwitterContainer>
+          <TwitterIcon />
+        </TwitterContainer> */}
+        <Row margin="10% 0px 0px 0px">
+          <TwitterContainer>
+            <img src={TwitterIcon} alt="twitter icon" />
+            <TwitterHandle>{`@${user.twitterHandle}`}</TwitterHandle>
+          </TwitterContainer>
+          <WebsiteContainer>
+            <img src={GlobeIcon} alt="twitter icon" />
+          </WebsiteContainer>
+        </Row>
+        <Row margin="12% 0px 0px 0px">
+          <DescriptionText>{`${user.description}`}</DescriptionText>
         </Row>
       </RootContainer>
     </>
@@ -88,11 +115,14 @@ const RootContainer = styled.div`
   padding: 16px, 16px, 24px, 16px;
   background-color: #111111;
   margin: 3%;
+  padding-top: 22px;
+  padding-left: 22px;
 `;
 
 const Row = styled.div`
   display: flex;
   flex-direction: row;
+  margin: ${(props) => (props.margin ? props.margin : 0)};
 `;
 
 const Column = styled.div`
@@ -109,6 +139,8 @@ const LevelDiv = styled.div`
   justify-content: center;
   border-radius: 44px;
   padding: 8px;
+  padding-right: 0px;
+  padding-left: 0px;
   background-color: rgba(248, 169, 16, 0.1);
   font-family: "Sharp Grotesk";
   font-weight: 600;
@@ -130,6 +162,44 @@ const Subtitle = styled.div`
   color: #a2a2a2;
   align-self: flex-start;
   margin-top: 10px;
+`;
+
+const TwitterContainer = styled.div`
+  background-color: #1a1a1a;
+  border-radius: 24px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  color: #a2a2a2;
+  font-size: 0.8rem;
+  padding: 4px 8px 4px 8px;
+`;
+
+const SubtitleTextFontProps = styled.div`
+  font-family: "Sharp Grotesk Regular";
+  font-weight: 400;
+  font-size: 0.8rem;
+  color: #a2a2a2;
+`;
+
+const TwitterHandle = styled(SubtitleTextFontProps)`
+  margin-left: 9px;
+`;
+
+const DescriptionText = styled(SubtitleTextFontProps)`
+  font-size: 0.9rem;
+  padding-right: 10%;
+`;
+
+const WebsiteContainer = styled.div`
+  border-radius: 24px;
+  background-color: #1a1a1a;
+  padding: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 5%;
 `;
 
 export default LeftPane;
